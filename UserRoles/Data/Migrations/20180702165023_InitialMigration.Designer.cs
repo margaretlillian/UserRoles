1525089847bac2546322821cc5de0df5b3e79985
Margaret Kiley <mlilliankiley@gmail.com>
2018-07-02 12:52:56 -0400

    Initial commit

--- UserRoles/Data/Migrations/20180702165023_InitialMigration.Designer.cs
+++ UserRoles/Data/Migrations/20180702165023_InitialMigration.Designer.cs
@@ -11,9 +11,10 @@ using UserRoles.Data;
 namespace UserRoles.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180702165023_InitialMigration")]
+    partial class InitialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
